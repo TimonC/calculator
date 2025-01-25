e7feef6bfca9e8a88a0992a657c7472df6c5ad92
TCrouzen <timon.crouzen@gmail.com>
2025-01-25 19:10:48 +0100

    Button flex layout
    Fixed the button layout, now they are flexboxed nicely to fill out the calculator format. Time to add functionality!

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let display = document.createElement('div');
 let keyboard = document.createElement('div');
 let inputs = document.createElement('div')
 let operators = document.createElement('div')
+let numbers = document.createElement('div');
+let extras= document.createElement('div');
 
 
 body.appendChild(calculator);
@@ -11,39 +13,35 @@ calculator.appendChild(display);
 calculator.appendChild(keyboard);
 keyboard.appendChild(inputs);
 keyboard.appendChild(operators);
+inputs.appendChild(numbers);
+inputs.appendChild(extras);
 
 calculator.id='calculator';
 
-display.style.width=`95%`;
+display.id='display';
+display.style.width=`98%`;
 display.style.height=`20%`;
-display.style.backgroundColor=`gray`;
-display.margin=`1em`;
-display.style.textAlign=`right`;
 
-keyboard.style.display='flex';
+keyboard.classList.add('button-container')
 keyboard.style.width=`100%`;
 keyboard.style.height=`80%`;
 
-inputs.style.display='flex';
+inputs.classList.add('button-container')
 inputs.style.flexDirection='column';
-inputs.style.maxWidth=`75%`;
-inputs.style.maxHeight=`100%`;
+inputs.style.width=`75%`;
 
-operators.style.display='flex';
+operators.classList.add('button-container')
 operators.style.flexDirection='column';
-operators.style.maxWidth=`25%`;
-operators.style.maxHeight=`100%`;
+operators.style.width=`25%`;
 
 
 
-
-let numbers = document.createElement('div');
-numbers.style.display='flex';
+numbers.classList.add('button-container')
+numbers.style.height=`80%`
 numbers.style.flexWrap = 'wrap-reverse';
 
-let extras= document.createElement('div');
-extras.style.display='flex';
-
+extras.classList.add('button-container')
+extras.style.height=`20%`
 inputs.appendChild(extras);
 inputs.appendChild(numbers);
 
@@ -53,24 +51,40 @@ inputs.appendChild(numbers);
 let extraList = ['AC', '+/-', '%'];
 let operatorList = ['/', '*', '-', '+', '='];
 let numberList = ['0', '.'];
-for (let i =1; i<8; i++){
+for (let i =1; i<10; i++){
     numberList.push(''+i);
 }
 for (let iNumber =0; iNumber<numberList.length; iNumber++){
+    let span = document.createElement('span');
+    span.textContent = numberList[iNumber];
+
     let button = document.createElement('button');
-    button.classList.add('number-button');
-    button.textContent = numberList[iNumber];
+    if(numberList[iNumber]=='0'){
+        button.style.minWidth=`22em`;
+        button.style.flex= `2 1 auto`;
+    }
+    button.style.backgroundColor=`teal`;
+
+    button.appendChild(span);
     numbers.appendChild(button);
 }
+
 for (let iExtra =0; iExtra<extraList.length; iExtra++){
+    let span = document.createElement('span');
+    span.textContent = extraList[iExtra];
+
     let button = document.createElement('button');
-    button.classList.add('extra-button');
-    button.textContent = extraList[iExtra]
+    button.style.backgroundColor=`blue`;
+
+    button.appendChild(span);
     extras.appendChild(button);
 }
 for (let iOperator =0; iOperator<operatorList.length; iOperator++){
+    let span = document.createElement('span');
+    span.textContent = operatorList[iOperator];
     let button = document.createElement('button');
-    button.classList.add('operator-button');
-    button.textContent = operatorList[iOperator]
+    button.style.backgroundColor=`orange`;
+
+    button.appendChild(span);
     operators.appendChild(button);
 }
