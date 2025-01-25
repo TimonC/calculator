394375023f08d915fa36576740c50dae291de86b
TCrouzen <timon.crouzen@gmail.com>
2025-01-25 21:14:18 +0100

    Added functionality between buttons and display

--- script.js
+++ script.js
@@ -1,49 +1,113 @@
 const body = document.querySelector('body');
-let calculator = document.createElement('div');
+
+let calcContainer = document.createElement('div');
+calcContainer.id='calculator';
+
 let display = document.createElement('div');
-let keyboard = document.createElement('div');
-let inputs = document.createElement('div')
-let operators = document.createElement('div')
-let numbers = document.createElement('div');
-let extras= document.createElement('div');
-
-
-body.appendChild(calculator);
-calculator.appendChild(display);
-calculator.appendChild(keyboard);
-keyboard.appendChild(inputs);
-keyboard.appendChild(operators);
-inputs.appendChild(numbers);
-inputs.appendChild(extras);
-
-calculator.id='calculator';
-
+display.textContent = '0';
 display.id='display';
 display.style.width=`98%`;
 display.style.height=`20%`;
 
+let keyboard = document.createElement('div');
 keyboard.classList.add('button-container')
 keyboard.style.width=`100%`;
 keyboard.style.height=`80%`;
 
+let inputs = document.createElement('div')
 inputs.classList.add('button-container')
 inputs.style.flexDirection='column';
-inputs.style.width=`75%`;
 
+let operators = document.createElement('div')
 operators.classList.add('button-container')
 operators.style.flexDirection='column';
-operators.style.width=`25%`;
 
-
-
+let numbers = document.createElement('div');
 numbers.classList.add('button-container')
 numbers.style.height=`80%`
 numbers.style.flexWrap = 'wrap-reverse';
 
+let extras= document.createElement('div');
 extras.classList.add('button-container')
 extras.style.height=`20%`
+
+
+body.appendChild(calcContainer);
+calcContainer.appendChild(display);
+calcContainer.appendChild(keyboard);
+keyboard.appendChild(inputs);
+keyboard.appendChild(operators);
 inputs.appendChild(extras);
 inputs.appendChild(numbers);
+
+
+
+const calculator = {
+    displayNumber: '',
+    prev: 0,
+    oper: null,
+    newNumber : true,
+    numInsert: function(sign){
+        if (!this.newNumber){ 
+            this.displayNumber = this.displayNumber + sign;
+        }
+        else{
+            this.displayNumber = sign;
+            this.newNumber=false;
+        }
+        },
+
+    operator: function(sign){
+        this.newNumber=true;
+        if (this.oper==null){
+            this.oper=sign;
+            this.prev=Number(this.displayNumber);
+            if (sign=='='){
+                this.oper=null;
+                this.prev=0;
+            }
+        }
+        else{
+            switch (this.oper){
+                    case '/':
+                        this.displayNumber = '' + (this.prev / Number(this.displayNumber))
+                        break;
+                    case '*':
+                        this.displayNumber = '' + (this.prev * Number(this.displayNumber))
+                        break;
+                    case '-':
+                        this.displayNumber = '' + (this.prev - Number(this.displayNumber))
+                        break;
+                    case '+':
+                        this.displayNumber = '' + (this.prev + Number(this.displayNumber))
+                        break;
+                    default:
+                        console.error("invalid operator sign")
+                }
+                this.oper=null;
+                this.prev=0;
+            }
+        },
+    
+    extra: function(sign){
+        switch (sign){
+            case 'AC':
+                displayNumber = '';
+                prev = 0;
+                oper = null;
+                newNumber = true;
+                break;
+            case '+/-':
+                this.displayNumber = '-' + this.displayNumber;
+                break;
+            case '%':
+                this.displayNumber = '' + Number(this.displayNumber)/100;
+                break;
+            default:
+                console.error("invalid extras sign")
+        }
+    },
+}
 
 
 
@@ -57,13 +121,16 @@ for (let i =1; i<10; i++){
 for (let iNumber =0; iNumber<numberList.length; iNumber++){
     let span = document.createElement('span');
     span.textContent = numberList[iNumber];
-
     let button = document.createElement('button');
     if(numberList[iNumber]=='0'){
         button.style.minWidth=`22em`;
         button.style.flex= `2 1 auto`;
     }
     button.style.backgroundColor=`teal`;
+
+    button.addEventListener("click", function() {
+        calculator.numInsert(numberList[iNumber]);
+        display.textContent=calculator.displayNumber;});
 
     button.appendChild(span);
     numbers.appendChild(button);
@@ -72,10 +139,14 @@ for (let iNumber =0; iNumber<numberList.length; iNumber++){
 for (let iExtra =0; iExtra<extraList.length; iExtra++){
     let span = document.createElement('span');
     span.textContent = extraList[iExtra];
-
     let button = document.createElement('button');
     button.style.backgroundColor=`blue`;
 
+
+    button.addEventListener("click", function() {
+        calculator.extra(extraList[iExtra]);
+        display.textContent=calculator.displayNumber;});
+        
     button.appendChild(span);
     extras.appendChild(button);
 }
@@ -85,6 +156,14 @@ for (let iOperator =0; iOperator<operatorList.length; iOperator++){
     let button = document.createElement('button');
     button.style.backgroundColor=`orange`;
 
+
+    button.addEventListener("click", function() {
+        calculator.operator(operatorList[iOperator]);
+        display.textContent=calculator.displayNumber;});
+
     button.appendChild(span);
     operators.appendChild(button);
 }
+
+
+
