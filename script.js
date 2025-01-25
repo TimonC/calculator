63f1d6a03f7c8d9d9c1a6fe696eb5c5136159244
TCrouzen <timon.crouzen@gmail.com>
2025-01-25 17:17:26 +0100

    Button allignments and moving CSS to JS
    I made the different button group roughly formatted the way they should, although there are issues between alligning the button groups and with empty space remaining. Furthermore, I moved some CSS declarations to JS for consistency and clarity.

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
-let leftSide = document.querySelector('#leftside');
-let rightSide = document.querySelector('#rightside');
+const body = document.querySelector('body');
+let calculator = document.createElement('div');
+let display = document.createElement('div');
+let keyboard = document.createElement('div');
+let inputs = document.createElement('div')
+let operators = document.createElement('div')
+
+
+body.appendChild(calculator);
+calculator.appendChild(display);
+calculator.appendChild(keyboard);
+keyboard.appendChild(inputs);
+keyboard.appendChild(operators);
+
+calculator.id='calculator';
+
+display.style.width=`95%`;
+display.style.height=`20%`;
+display.style.backgroundColor=`gray`;
+display.margin=`1em`;
+display.style.textAlign=`right`;
+
+keyboard.style.display='flex';
+keyboard.style.width=`100%`;
+keyboard.style.height=`80%`;
+
+inputs.style.display='flex';
+inputs.style.flexDirection='column';
+inputs.style.maxWidth=`75%`;
+inputs.style.maxHeight=`100%`;
+
+operators.style.display='flex';
+operators.style.flexDirection='column';
+operators.style.maxWidth=`25%`;
+operators.style.maxHeight=`100%`;
+
+
+
+
 let numbers = document.createElement('div');
-numbers.classList.add('button-container');
-numbers.flexWrap = 'wrap-reverse';
+numbers.style.display='flex';
+numbers.style.flexWrap = 'wrap-reverse';
 
 let extras= document.createElement('div');
+extras.style.display='flex';
 
-let operators= document.createElement('div');
-
-leftSide.appendChild(extras);
-leftSide.appendChild(numbers);
-rightSide.appendChild(operators);
+inputs.appendChild(extras);
+inputs.appendChild(numbers);
 
 
 
@@ -18,24 +53,24 @@ rightSide.appendChild(operators);
 let extraList = ['AC', '+/-', '%'];
 let operatorList = ['/', '*', '-', '+', '='];
 let numberList = ['0', '.'];
-for (let i =1; i<10; i++){
+for (let i =1; i<8; i++){
     numberList.push(''+i);
 }
-for (number in numberList){
+for (let iNumber =0; iNumber<numberList.length; iNumber++){
     let button = document.createElement('button');
     button.classList.add('number-button');
-    button.textContent = number;
+    button.textContent = numberList[iNumber];
     numbers.appendChild(button);
 }
-for (extra in extraList){
+for (let iExtra =0; iExtra<extraList.length; iExtra++){
     let button = document.createElement('button');
     button.classList.add('extra-button');
-    button.textContent = extra;
+    button.textContent = extraList[iExtra]
     extras.appendChild(button);
 }
-for (operator in operatorList){
+for (let iOperator =0; iOperator<operatorList.length; iOperator++){
     let button = document.createElement('button');
     button.classList.add('operator-button');
-    button.textContent = operator;
+    button.textContent = operatorList[iOperator]
     operators.appendChild(button);
 }
